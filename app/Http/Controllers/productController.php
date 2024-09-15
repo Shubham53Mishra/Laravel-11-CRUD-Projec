7ddf6d5fe7bf7f6162df5b4649c39260aaab0557
Shubham mishra <142476256+Shubham53Mishra@users.noreply.github.com>
2024-09-15 17:03:19 +0530

    add file

---
+++ app/Http/Controllers/productController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class productController extends Controller
+{
+    //This  method will show products page
+    public function index() {
+
+    }
+    // this method will show  create product page
+    public function create() {
+
+    }
+    // this  method will store a products in db
+    public function store() {
+
+    }
+  //  this method will show  edit product page
+    public function edit() {
+
+    }
+    //  this method will update a product
+    public function update() {
+
+    }
+    //  this method will  delete a product
+    public function destroy() {
+
+    }
+}
